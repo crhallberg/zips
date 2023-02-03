831d60adb4b4d1fce0cea8c3367ebe0a9220715d
Chris Hallberg <chris.hallberg@hey.com>
2023-02-03 12:45:15 -0500

    update: update to latest GeoNames data.

--- test/by-location.js
+++ test/by-location.js
@@ -3,14 +3,14 @@ const zips = require('../index.js');
 
 describe('Valid locations', function() {
   it('correct when spot on', function() {
-    let place = zips.getByZipCode('19026');
+    let place = zips.getByZipCode('19085');
     let locPlace = zips.getByLocation(place.lat, place.long);
-    assert.equal('19026', locPlace.zip);
+    assert.equal('19085', locPlace.zip);
   });
   it('correct when slightly off', function() {
-    let place = zips.getByZipCode('19026');
+    let place = zips.getByZipCode('19085');
     let locPlace = zips.getByLocation(place.lat + 0.000001, place.long + 0.000001);
-    assert.equal('19026', locPlace.zip);
+    assert.equal('19085', locPlace.zip);
   });
   it('return multiple closest when asked', function() {
     let count = 3;
@@ -22,9 +22,9 @@ describe('Valid locations', function() {
     // Safe band: 35 to 40 lat, -120 to -80 long
     // http://www.okatlas.org/okatlas/location/usa/lat-long.gif
     for (let i = 1; i <= randomLocCount; i++) {
-      const lat  = Math.random() *  5 +  35; //   35 to  40
-      const long = Math.random() * 40 - 120; // -120 to -80
-      assert.ok(zips.getByLocation(lat, long), 'no zipcodes near ' + lat + ', ' + long);
+      const lat  = 35 + Math.random() *  5;   //   35 to  40
+      const long = -120 + Math.random() * 40; // -120 to -80
+      assert.ok(zips.getByLocation(lat, long), `no zipcodes near ${lat},${long} (${i})`);
     }
   });
 });
